02b0743a5206f66d79a8adce1211b2e3f33768ba
Henry Cooper <cooperhj@cardiff.ac.uk>
2019-05-08 13:37:55 +0100

    Update account_creation.sql

--- sql/account_creation.sql
+++ sql/account_creation.sql
@@ -1,3 +1,3 @@
-create user 'mha'@'localhost' identified by 'strongpassword';
+create user 'mha'@'localhost' identified by 'Strongpassword1!';
 
 GRANT SELECT, INSERT, DELETE, EXECUTE ON mha_db.* TO 'mha'@'localhost';
